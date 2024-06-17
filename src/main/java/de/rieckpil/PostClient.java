f0e489e36f0b637eb700b5eed7d469f0f673b955
Paul Zijlmans <paul.zijlmans@ordina.nl>
2024-06-17 17:24:06 +0200

    Fetching Remote Data

--- src/main/java/de/rieckpil/PostClient.java
+++ src/main/java/de/rieckpil/PostClient.java
@@ -1,8 +1,46 @@
 package de.rieckpil;
 
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Develop an HTTP client that fetch all posts from the post-service and return them as a list. The
  * post-service returns the all posts with pagination. The client should fetch all pages and return
  * the result as a list.
  */
-public class PostClient {}
+@Component
+public class PostClient {
+
+  private final WebClient postWebClient;
+
+  public PostClient(WebClient postWebClient) {
+    this.postWebClient = postWebClient;
+  }
+
+  public List<Post> fetchAllPosts() {
+    int limit = 30;
+    int skip = 0;
+    long totalResult = 0;
+
+    List<Post> allPosts = new ArrayList<>();
+
+    do {
+      PostResult result = postWebClient.get()
+        .uri("/posts?limit={limit}&skip={skip}", limit, skip)
+        .accept(MediaType.APPLICATION_JSON)
+        .retrieve()
+        .bodyToMono(PostResult.class)
+        .block();
+
+      totalResult = result.total();
+      skip += limit;
+      allPosts.addAll(result.posts());
+    } while (allPosts.size() < totalResult);
+
+    return allPosts;
+  }
+}
---
+++ src/main/java/de/rieckpil/PostResult.java
@@ -0,0 +1,6 @@
+package de.rieckpil;
+
+import java.util.List;
+
+public record PostResult(List<Post> posts, long total, long skip, long limit) {
+}
